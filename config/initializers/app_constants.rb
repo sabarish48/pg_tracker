eb9bd3dfe4be92e36a41f738076a60617a79c0c7
sabarish48 <sabarishsankar89@yahoo.co.in>
2011-01-11 01:13:47 +0530

    change in email again

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,11 +1,11 @@
-module PGTracker
-  NAME  = "PGTracker"
-  EMAIL = "pgtracker@gmail.com"
-  FROM_EMAIL = "pgtracker@gmail.com"
-end
-
 module Trojans
   NAME  = "Trojans"
   EMAIL = "my_own_creation@googlegroups.com"
+  FROM_EMAIL = "trojans41.mailer@gmail.com"
+end
+
+module PGTracker
+  NAME  = "PGTracker"
+  EMAIL = "trojans41.mailer@gmail.com"
   FROM_EMAIL = "trojans41.mailer@gmail.com"
 end
